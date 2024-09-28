b4b52ff46472d7cf0619ede4c9071bac01737ea5
rayanabonfanti <rayanabonfanti@gmail.com>
2024-09-28 13:34:38 -0300

    feat: add authentication with google in supabase and flutter

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -4,13 +4,17 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 Future<void> initializeSupabase() async {
   try {
     await dotenv.load(fileName: ".env");
+    // await Supabase.initialize(
+    //   url: dotenv.env['https://vkdmrqrkkyrrgxkjblzw.supabase.co']!,
+    //   anonKey: dotenv.env['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrZG1ycXJra3lycmd4a2pibHp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE4Njg0NTQsImV4cCI6MjAzNzQ0NDQ1NH0.qLXpCb53YRTBjSclCw7cRUNIskSDvxZIUu2G5jZm00s']!,
+    // );
     await Supabase.initialize(
-      url: dotenv.env['SUPABASE_URL']!,
-      anonKey: dotenv.env['SUPABASE_KEY']!,
+      url: 'https://vkdmrqrkkyrrgxkjblzw.supabase.co',
+      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrZG1ycXJra3lycmd4a2pibHp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE4Njg0NTQsImV4cCI6MjAzNzQ0NDQ1NH0.qLXpCb53YRTBjSclCw7cRUNIskSDvxZIUu2G5jZm00s',
     );
     print('Supabase initialized successfully');
   } catch (e) {
     print('Failed to initialize Supabase: $e');
-    rethrow; // Re-throw the error to handle it in the FutureBuilder
+    rethrow;
   }
 }
